476cab31b7e58ab7d6c722daa22b32b93b65e0b8
HWALIM <hwalim9612@gmail.com>
2024-01-07 16:01:08 +0900

    개념 마무리

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -2,10 +2,11 @@ import PropTypes from "prop-types";
 
 function MovieDetail({ movie }) {
   return (
+    // img 태그의 alt 속성은 이미지를 보여줄 수 없을 떄 해당 이미지를 대체할 테스트를 명시하는 것
     <div>
       <img src={movie.background_image} alt={movie.title}></img>
-      <img src={movie.small_cover_image} alt={movie.title}></img>
       <div>
+        <img src={movie.small_cover_image} alt={movie.title}></img>
         <h2>{movie.title}</h2>
         <h3>{movie.rating}</h3>
         <p>{movie.description_intro}</p>
